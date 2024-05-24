f55427c6b5b7ba4793e3cf9be0406e46e69e98b1
Joaquin Franco <chino.franco@gmail.com>
2024-05-24 23:11:05 +0900

    fix: set layout to widescreen

--- infraboard/monitor.py
+++ infraboard/monitor.py
@@ -1,4 +1,5 @@
 import time
+from typing import Optional
 
 import streamlit as st
 
@@ -6,7 +7,7 @@ from .models import SystemMetrics, TimeSeriesData
 
 
 class InfraMonitor:
-    _configured = st.set_page_config(page_title="Dashboard")
+    _configured = st.set_page_config(layout="wide", page_title="Dashboard")
     _count = 0
 
     def __init__(self, interval_min: int, interval_max: int) -> None:
@@ -39,6 +40,10 @@ class InfraMonitor:
         print("Running system monitoring...")
         self.__render()
 
+    @staticmethod
+    def __bytes_to_mb(bytes: int, precision: Optional[int] = 2) -> float:
+        return round(bytes / (1024 * 1024), precision)
+
     def __render(self) -> None:
         self.__running = True
         st.title("Development Computer Metrics Monitor")
@@ -55,8 +60,13 @@ class InfraMonitor:
                     st.metric("CPU Usage (%)", self.metrics.cpu_usage)
                     st.metric("Memory Usage (%)", self.metrics.memory_usage)
                     st.metric("Disk Usage (%)", self.metrics.disk_usage)
-                    st.metric("Bytes Sent", self.metrics.bytes_sent)
-                    st.metric("Bytes Received", self.metrics.bytes_received)
+                    st.metric(
+                        "Data Sent (MB)", self.__bytes_to_mb(self.metrics.bytes_sent)
+                    )
+                    st.metric(
+                        "Data Received (MB)",
+                        self.__bytes_to_mb(self.metrics.bytes_received),
+                    )
                     st.line_chart(df.set_index("Time"))
 
                 time.sleep(self.__refresh_interval)
